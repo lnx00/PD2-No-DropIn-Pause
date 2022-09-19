96b6ee5b2948ec3f503af1acde1adb1bf3fba41f
lnx00 <linch255@gmail.com>
2022-09-19 02:43:25 +0200

    Fixed dofile calls

--- mod.lua
+++ mod.lua
@@ -1,6 +1,8 @@
 if not NDIP then
 
     NDIP = {
+        ModPath = ModPath,
+
         Settings = {
             Enabled = true, -- Prevent drop-in pause
             StealthPause = true -- Allows pause when stealth is active
@@ -19,7 +21,13 @@ if not NDIP then
         return managers.groupai and managers.groupai:state():whisper_mode()
     end
 
+    function NDIP:RequireFile(hook, file)
+        if string.lower(RequiredScript) == string.lower(hook) then
+            dofile(self.ModPath .. file)
+        end
+    end
+
 end
 
-if string.lower(RequiredScript) == "lib/network/base/basenetworksession" then dofile("lua/basenetworksession.lua") end
-if string.lower(RequiredScript) == "lib/managers/menumanagerdialogs" then dofile("lua/menumanagerdialogs.lua") end
+NDIP:RequireFile("lib/network/base/basenetworksession", "req/basenetworksession.lua")
+NDIP:RequireFile("lib/managers/menumanagerdialogs", "req/menumanagerdialogs.lua")
